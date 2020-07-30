b26c5e28d960124af11935b6ab560f972942ba3c
bemorr <benmorrisldnuk@gmail.com>
2020-07-31 01:15:46 +0200

    Adds limited styling and sqlite connection Exception handling.

--- index.php
+++ index.php
@@ -1,28 +1,43 @@
 <?php
+error_reporting(E_ALL);
+
 require 'vendor/autoload.php';
 
 use App\SQLiteConnection;
 
-$pdo = (new SQLiteConnection())->connect();
-if ($pdo != null)
-    echo 'Connected to the SQLite database successfully!';
-else
-    echo 'Whoops, could not connect to the SQLite database!'
+try {
+    $pdo = (new SQLiteConnection())->connect();
+    echo 'Successful connection to SQLite database established!';
+} catch (\PDOException $e) {
+    var_dump($e);
+    trigger_error('SQLite3 Fail: ' . $e->getMessage(), E_USER_ERROR);
+}
 
 ?>
 
-<form method="post" action="message.php">
-    <h1><b>Bunq Chat app</b></h1>
+<head>
+    <title>BunqsApp Users Login</title>
+    <style>
+        body {
+            background-image: url('./public_html/assets/bunq-login-img.png');
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-position: center;
+        }
+    </style>
+</head>
+<form method="post" id="chat_credentials" action="app/messaging_interface.php">
+    <h1 style="background-color: white; width:150px"><b>BunqsApp</b></h1>
     <div>
-        <label for="username">Create a username for yourself:</label>
+        <label for="my_username">Create a username for yourself:</label>
         </br>
-        <input type="text" name="myUn" value="" id="username" required="required">
+        <input type="text" name="my_user" value="" id="myUsername" required="required">
     </div>
     </br></br>
     <div>
-        <label for="friendU/N">Enter username of friend you want to message:</label>
+        <label for="friend_username">Enter username of friend you want to message:</label>
         </br>
-        <input type="text" name="fUn" value="" id="friendUsername" required="required">
+        <input type="text" name="f_user" value="" id="fUsername" required="required">
     </div>
     </br></br>
     <div>
